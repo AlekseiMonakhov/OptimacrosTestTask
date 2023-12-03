b14ffe6e54d899df095e37350a3f0dc87e46d03a
Aleksei Monakhov <lexmonakhov@yandex.ru>
2023-12-04 05:19:41 +0530

    inicialisation and setting server app

---
+++ server/index.ts
@@ -0,0 +1,31 @@
+import express, { Express } from 'express';
+import dotenv from 'dotenv';
+import mongoose from 'mongoose';
+import authRouter from './routes/authRouter';
+import carsRouter from './routes/carsRouter';
+
+dotenv.config();
+
+const app: Express = express();
+const port = process.env.SERVER_PORT;
+const host = process.env.SERVER_HOST;
+if (!process.env.MONGO_URI) {
+  throw new Error('MONGO_URI must be defined');
+}
+const mongoUri = process.env.MONGO_URI as string;
+
+app.use(express.json());
+app.use('/api/v1', [authRouter, carsRouter]);
+
+
+mongoose.connect(mongoUri)
+  .then(() => {
+    console.log('Connected to MongoDB');
+    app.listen(port, () => {
+      console.log(`⚡️[server]: Server is running at http://${host}:${port}`);
+    });
+  })
+  .catch(err => {
+    console.error('Error connecting to MongoDB:', err);
+  });
+
